c14d4200096ae2f15ad54092e77fa46310dd676c
AnastasiaBro <a-sirion@yandex.ru>
2017-11-23 14:50:33 +0300

    Функция для создания массива магов

--- js/setup.js
+++ js/setup.js
@@ -9,32 +9,22 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var wizards = [
-  {
-    name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomIndex(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomIndex(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomIndex(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomIndex(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomIndex(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomIndex(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomIndex(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomIndex(0, EYES_COLORS.length - 1)]
-  }
-];
+var NUMBER_OF_WIZARDS = 4;
+var wizards = [];
 
 function getRandomIndex(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function getArrayWizard(wizardNumber) {
+  for (var j = 0; j < wizardNumber; j++) {
+    wizards.push({
+      name: WIZARD_NAMES[getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomIndex(0, WIZARD_SURNAMES.length - 1)],
+      coatColor: COAT_COLORS[getRandomIndex(0, COAT_COLORS.length - 1)],
+      eyesColor: EYES_COLORS[getRandomIndex(0, EYES_COLORS.length - 1)]
+    });
+  }
+  return wizards;
 }
 
 function drawCloneWizards(wizard) {
@@ -49,6 +39,7 @@ function drawCloneWizards(wizard) {
 
 (function insertWizards() {
   var fragment = document.createDocumentFragment();
+  getArrayWizard(NUMBER_OF_WIZARDS);
   for (var i = 0; i < wizards.length; i++) {
     fragment.appendChild(drawCloneWizards(wizards[i]));
   }
