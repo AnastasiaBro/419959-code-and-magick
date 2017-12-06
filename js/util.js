3b3db4d340d3acc227b73fd08527ca1fcd224243
AnastasiaBro <a-sirion@yandex.ru>
2017-12-07 02:03:44 +0300

    Модули

--- js/util.js
+++ js/util.js
@@ -1,33 +1,35 @@
 // Файл util.js
 'use strict';
 
-window.util = (function () {
+(function () {
+  window.util = (function () {
 
-  return {
-    isEscEvent: function (evt, action) {
-      if (evt.keyCode === window.ESC_KEYCODE) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === window.ENTER_KEYCODE) {
-        action();
-      }
-    },
-    isRandomNumber: function (min, max) {
-      return Math.floor(min + Math.random() * (max + 1 - min));
-    },
-    isOpacity: function () {
-      return Math.ceil(Math.random() * 10) / 10;
-    },
-    isSearchMax: function (maxValue, array) {
-      for (var i = 0; i < array.length; i++) {
-        var value = array[i];
-        if (value > maxValue) {
-          maxValue = value;
+    return {
+      isEscEvent: function (evt, action) {
+        if (evt.keyCode === window.ESC_KEYCODE) {
+          action();
         }
+      },
+      isEnterEvent: function (evt, action) {
+        if (evt.keyCode === window.ENTER_KEYCODE) {
+          action();
+        }
+      },
+      isRandomNumber: function (min, max) {
+        return Math.floor(min + Math.random() * (max + 1 - min));
+      },
+      isOpacity: function () {
+        return Math.ceil(Math.random() * 10) / 10;
+      },
+      isSearchMax: function (maxValue, array) {
+        for (var i = 0; i < array.length; i++) {
+          var value = array[i];
+          if (value > maxValue) {
+            maxValue = value;
+          }
+        }
+        return maxValue;
       }
-      return maxValue;
-    }
-  };
+    };
+  })();
 })();
